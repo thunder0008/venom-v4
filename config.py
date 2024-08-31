a82b2fcf4f29daa6464711648e4ef53d22ec33e9
thunder0008 <kawana4@lyalnorm.com>
2024-08-31 19:39:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7509525381:AAHneBr44-I89T68dkQwcWON9di3d2QKPfs")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("unknown_2863")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5139930247")
 
 
 
